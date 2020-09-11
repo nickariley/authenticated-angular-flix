3ca63094ed462a4b4bbafd9c5f05a53f2624d1e0
nickariley <nickariley@gmail.com>
2020-09-11 17:37:05 -0500

    signup login component added app routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupLoginComponent } from './signup-login/signup-login.component';
+import { MainComponent } from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SignupLoginComponent},
+  {path: 'main', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/signup-login/signup-login.component.ts
+++ src/app/signup-login/signup-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-login',
@@ -7,21 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupLoginComponent implements OnInit {
 
+  user: {
+    username: string,
+    password: string
+  };
 
-  username: string;
-  password: string;
+  invalidLogin = false;
 
-  constructor() { }
+
+  constructor(private authService: AuthenticationService, private router: Router) {
+    this.user = {
+      username: '',
+      password: ''
+    };
+  }
 
   ngOnInit() {
   }
 
-  login() {
+  async login(): Promise<void> {
+    await this.authService.login(this.user.username, this.user.password);
 
+    if (this.authService.getToken === undefined) {
+      this.invalidLogin = true;
+    } else {
+      this.invalidLogin = false;
+      this.router.navigate(['/main']);
+    }
   }
 
-  signup() {
-    
+  async signup(): Promise<void> {
+    await this.authService.signup(this.user.username, this.user.password);
+    await this.login();
   }
 
 }
