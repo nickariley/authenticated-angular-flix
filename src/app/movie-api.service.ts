71fceba373b3a914d67cb08484394df70953d505
nickariley <nickariley@gmail.com>
2020-09-11 11:04:19 -0500

    corrected linting errors

--- src/app/movie-api.service.ts
+++ src/app/movie-api.service.ts
@@ -4,10 +4,10 @@ import { HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class MovieApiService {
 
-  constructor(private httpClient : HttpClient) { }
-  url = "https://api.themoviedb.org/3/"
-  async get(path, options?): Promise<any>{
-    return await this.httpClient.get(this.url + path + "&api_key=951c4e8a331e2637479312da880d05c8",options).toPromise();
+  constructor(private httpClient: HttpClient) { }
+  url = 'https://api.themoviedb.org/3/';
+  async get(path, options?): Promise<any> {
+    return await this.httpClient.get(this.url + path + '&api_key=951c4e8a331e2637479312da880d05c8', options).toPromise();
   }
 
 }
